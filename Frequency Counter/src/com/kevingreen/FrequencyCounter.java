3d53c34fba2de12bb6fbc6464f426b906993ec47
Kevin Green <kevin.e.green@hotmail.com>
2018-11-29 21:36:56 -0600

    Java Structures

--- Frequency Counter/src/com/kevingreen/FrequencyCounter.java
+++ Frequency Counter/src/com/kevingreen/FrequencyCounter.java
@@ -6,14 +6,22 @@ import java.util.Map;
 public class FrequencyCounter {
     public boolean sameFrequency(int num1, int num2){
         String firstNum = Integer.toString(num1);
-        String secondNum = Integer.toString(num2);
-        HashMap<Character, Character> frequency = new HashMap<Character, Character>();
+        HashMap<Character, Integer> frequency = new HashMap<>();
+        char testCase;
+        int count;
         for (int i = 0; i < firstNum.length(); i++){
-            frequency.put(firstNum.charAt(i), firstNum.charAt(i));
+            testCase = firstNum.charAt(i);
+            if (frequency.containsKey(testCase)){
+                count = frequency.get(testCase);
+                frequency.put(testCase, count +1);
+            }
+            else {
+                frequency.put(testCase, 1);
+            }
         }
 
-        for (Map.Entry<Character, Character> each: frequency.entrySet()){
-            System.out.println(each.getKey());
+        for (Map.Entry<Character, Integer> each: frequency.entrySet()){
+            System.out.println(each.getKey() +":"+each.getValue());
         }
         return true;
     }
--- Frequency Counter/src/com/kevingreen/Main.java
+++ Frequency Counter/src/com/kevingreen/Main.java
@@ -1,10 +1,10 @@
 package com.kevingreen;
 
+
 public class Main {
 
     public static void main(String[] args) {
-    FrequencyCounter test = new FrequencyCounter();
-
-    test.sameFrequency(1001231,23);
+        FrequencyCounter test = new FrequencyCounter();
+        test.sameFrequency(123113,1231);
     }
 }
